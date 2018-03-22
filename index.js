eda8c15ad148e9cc7aa6183c0b438f728a65fbe7
Logan <35646791+HunterWilson56@users.noreply.github.com>
2018-03-21 21:36:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
+client.on("ready", function() {
+    var clientonmessage = `
+------------------------------------------------------
+> Logging in...
+------------------------------------------------------
+Logged in as ${client.user.tag}
+Working on ${client.guilds.size} servers!
+${client.channels.size} channels and ${client.users.size} users cached!
+I am logged in and ready to roll!
+LET'S GO!
+------------------------------------------------------
+----------Bot created by Tea Cup#3433-----------
+------------------------------------------------------
+-----------------Bot's commands logs------------------`
+
+    console.log(clientonmessage);
+    client.user.setGame(
+        ` Test Bot For Hunters Youtube! `,
+        "https://www.twitch.tv/xxwilsongamingxx"
+      );
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login('SUPER_SECRETE_BOTLOGIN!'); //Thats it for todays lession! 3/21/18
